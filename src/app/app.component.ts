d4724afe1180a80c8a83ed4b66a8bd09ff00a13e
Human Flesh Body World <hfbw@Humans-iMac.domain>
2018-03-30 12:27:05 -0700

    format chart list component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,13 +11,19 @@ export class AppComponent {
   selectedChart = null;
 
   masterChartList: Chart[] = [
-
-
     new Chart('Nico Daunt', '06-05-1991', '08:26:00', '33.6188829 |-117.9298493')
   ];
 
   addChart(newChart: Chart) {
     this.masterChartList.push(newChart);
+  }
+
+  editChart(clickedChart) {
+  this.selectedChart = clickedChart;
+  }
+
+  finishedEditing() {
+  this.selectedChart = null;
   }
 
 }
--- src/app/chart-list/chart-list.component.ts
+++ src/app/chart-list/chart-list.component.ts
@@ -1,15 +1,20 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, Output, EventEmitter, OnInit } from '@angular/core';
+import { Keg } from '../models/chart.model';
 
 @Component({
   selector: 'app-chart-list',
   templateUrl: './chart-list.component.html',
   styleUrls: ['./chart-list.component.css']
 })
-export class ChartListComponent implements OnInit {
+export class ChartListComponent {
+  @Input() childChartList: Chart[];
+  @Output() clickedEdit = new EventEmitter();
+
+  editButtonClicked(chartToEdit: Chart) {
+   this.clickedEdit.emit(chartToEdit);
+ }
 
   constructor() { }
 
-  ngOnInit() {
-  }
 
 }
